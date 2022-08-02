decf892b7c98578cc09b250346a05646d23f73ec
johnc13b <100248387+johnc13b@users.noreply.github.com>
2022-08-01 22:27:04 -0400

    content

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import Nav from './components/Nav';
+import About from './components/About';
+import Gallery from './components/Gallery';
+import ContactForm from './components/Contact';
 
 function App() {
+  const [categories] = useState([
+    {
+      name: 'hobbies',
+      description: 'A small glimpse of the hobbies of a simple man',
+    },
+    {
+      name: 'tech',
+      description: 'Some newly acquired skills',
+    },
+    // { name: 'food', description: 'Delicious delicacies' },
+    // { name: 'landscape', description: 'Fields, farmhouses, waterfalls, and the beauty of nature' },
+  ]);
+
+  const [currentCategory, setCurrentCategory] = useState(categories[0]);
+  const [contactSelected, setContactSelected] = useState(false);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Nav
+        categories={categories}
+        setCurrentCategory={setCurrentCategory}
+        currentCategory={currentCategory}
+        contactSelected={contactSelected}
+        setContactSelected={setContactSelected}
+      ></Nav>
+      <main>
+        {!contactSelected ? (
+          <>
+            <About></About>
+            <Gallery currentCategory={currentCategory}></Gallery>
+          </>
+        ) : (
+          <ContactForm></ContactForm>
+        )}
+      </main>
     </div>
   );
 }
---
+++ src/components/PhotoList/index.js
@@ -0,0 +1,92 @@
+import React, { useState } from 'react';
+import Modal from '../Modal';
+
+const PhotoList = ({ category }) => {
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    const [currentPhoto, setCurrentPhoto] = useState();
+
+    const [photos] = useState([
+        {
+            name: 'Climbing',
+            category: 'hobbies',
+            description:
+                'A man looking at a wall full of problems'
+        },
+        {
+            name: 'Custom PC',
+            category: 'hobbies',
+            description:
+                'Full custom PC build'
+        },
+        {
+            name: 'Custumized PC',
+            category: 'hobbies',
+            description:
+                'Custumizing PC cases & compontents'
+        },
+        {
+            name: 'Content Creation',
+            category: 'hobbies',
+            description:
+                'Record, Edit, and Create videos for online entertainment'
+        },
+        {
+            name: 'Mechanicals',
+            category: 'hobbies',
+            description:
+                'Automotive mechanics by day, old school motorcycles by night'
+        },
+        {
+            name: 'HTML & CSS',
+            category: 'tech',
+            description:
+                'An introduction to coding'
+        },
+        {
+            name: 'Portfolio - Basic',
+            category: 'tech',
+            description:
+                'HTML & CSS based portfolio'
+        },
+        {
+            name: 'JavaScript & API',
+            category: 'tech',
+            description:
+                'An introduction to API calls'
+        },
+        {
+            name: 'Node & MYSQL',
+            category: 'tech',
+            description:
+                'An introduction to Node & MYSQL'
+        },
+    ]);
+
+    const currentPhotos = photos.filter(photo => photo.category === category);
+
+    const toggleModal = (image, i) => {
+        setCurrentPhoto({ ...image, index: i });
+        setIsModalOpen(!isModalOpen);
+    };
+
+    return (
+        <div>
+            {isModalOpen && (
+                <Modal onClose={toggleModal} currentPhoto={currentPhoto} />
+            )}
+            <div className="flex-row">
+                {currentPhotos.map((image, i) => (
+                    <img
+                        src={require(`../../assets/small/${category}/${i}.png`).default}
+                        alt={image.name}
+                        className="img-thumbnail mx-1"
+                        onClick={() => toggleModal(image, i)}
+                        key={image.name}
+                    />
+                ))}
+            </div>
+        </div>
+    );
+};
+
+export default PhotoList;
